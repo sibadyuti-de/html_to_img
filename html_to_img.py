1c91c7f12ba219747a3673229778dfde0914f57a
Sibadyuti De <45902123+sibadyuti-de@users.noreply.github.com>
2023-04-05 13:09:48 +0530

    Add files via upload

---
+++ html_to_img.py
@@ -0,0 +1,37 @@
+import imgkit         # refer to the documentation https://pypi.org/project/imgkit/ requires https://wkhtmltopdf.org/downloads.html
+
+options = {
+    'format': 'png'
+}
+
+#imgkit.from_url('https://google.com', 'google.png', options=options)  # from url
+
+imgkit.from_file('test.html', 'test.png', options=options)   # when supplied from file
+
+# or
+
+# with open('file.html') as f:
+#     imgkit.from_file(f, 'out.png')
+
+
+
+# body = """
+# <html>
+#   <head>
+#     <meta name="imgkit-format" content="png"/>
+#     <meta name="imgkit-orientation" content="Landscape"/>
+#   </head>
+#   Hello World!
+# </html>
+# """
+
+# imgkit.from_string(body, 'out.png')    # when use from strings  #instead of body string we can also use curl in linux 
+
+
+# Single CSS file
+# css = 'example.css'               # specified single css file
+# imgkit.from_file('file.html', options=options, css=css)
+
+# Multiple CSS files
+# css = ['example.css', 'example2.css']     # specified multiple css file
+# imgkit.from_file('file.html', options=options, css=css)
